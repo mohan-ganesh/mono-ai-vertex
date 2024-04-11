453f862c9959b4e12cf1ed48d8899ecf710f2b3f
Ganesh, Mohan <mohanganesh@gmail.com>
2024-04-11 09:13:09 -0400

    added banner background

--- web-conversation/src/environments/environment.ts
+++ web-conversation/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    conversation_api_url: 'https://localhost:3000/api/v1/conversation'
+    //conversation_api_url: 'https://vertex-serach-api-sn5rsq6dda-uc.a.run.app/api/v1/conversation'
+    conversation_api_url: "https://vertex-serach-api-zqr4b4s3sa-uc.a.run.app/api/v1/conversation"
 };
