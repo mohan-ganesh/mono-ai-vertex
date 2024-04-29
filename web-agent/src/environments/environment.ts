b765576f3096248874844624f6e4821bfe824e00
Ganesh, Mohan <mohanganesh@gmail.com>
2024-04-29 19:00:10 -0400

    header and api endpoint

--- web-agent/src/environments/environment.ts
+++ web-agent/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     //conversation_api_url: 'https://vertex-serach-api-sn5rsq6dda-uc.a.run.app/api/v1/conversation'
     //conversation_api_url: "https://test.conversation.goengen.com/api/v1/conversation"
-    conversation_api_url: "http://localhost:3000/api/v1/agent"
+    //conversation_api_url: "http://localhost:3000/api/v1/agent"
+    conversation_api_url: "https://test.helpdesk.goengen.com/api/v1/agent"
 };
